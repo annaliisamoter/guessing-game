f95d4311830704c5d1827a1a70b4d539ce99be9e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-22 15:15:50 -0700

    Added exceptions handling

--- game.py
+++ game.py
@@ -17,20 +17,24 @@ greeting = raw_input("Hello, what's your name? ")
 random_number = random.randint(1,100)
 count = 0
 while True:
+    try:
 
-    get_guess = int(raw_input("Guess a number between 1 and 100: "))
-    if get_guess not in range(1, 101):
-        print "You Fool! That is not an acceptable number!"
+        get_guess = int(raw_input("Guess a number between 1 and 100: "))
 
-    else:
-        if get_guess != random_number:
-            count += 1
-            if get_guess > random_number:
-                print "your guess is too high, try again"
-            else:
-                print "your guess is too low, try again"
+        if get_guess not in range(1, 101):
+            print "You Fool! That is not an acceptable number!"
+
         else:
-            print "Well done, {}!  You found my number in {} tries!".format(
-                greeting, count)
-            break
-
+            if get_guess != random_number:
+                count += 1
+                if get_guess > random_number:
+                    print "your guess is too high, try again"
+                else:
+                    print "your guess is too low, try again"
+            else:
+                print "Well done, {}!  You found my number in {} tries!".format(
+                    greeting, count)
+                break
+    except ValueError:
+        print ("Oops! That was not a valid number. Try again...")
+        pass
